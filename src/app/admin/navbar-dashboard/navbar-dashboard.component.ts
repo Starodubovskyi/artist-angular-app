9054933ce3f226d5ca1be3233a087562ba32e0fa
Yulian <hard.guzanu@gmail.com>
2023-01-21 00:39:27 +0200

    last update

--- src/app/admin/navbar-dashboard/navbar-dashboard.component.ts
+++ src/app/admin/navbar-dashboard/navbar-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar-dashboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -15,6 +16,6 @@ export class NavbarDashboardComponent implements OnInit {
     localStorage.removeItem("token");
     localStorage.removeItem("expires_at");
     localStorage.removeItem("user");
-    window.location.reload();
+    this.router.navigate(['/auth/login'])
   }
 }
